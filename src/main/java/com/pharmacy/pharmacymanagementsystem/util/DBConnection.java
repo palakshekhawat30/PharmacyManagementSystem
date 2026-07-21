879e69c6f27ea12f4a8c5e82e2b872bcff4c2313
palakshekhawat30 <palakshekhawat30@gmail.com>
2026-07-21 14:54:04 +0530

    Update DBConnection.java

--- src/main/java/com/pharmacy/pharmacymanagementsystem/util/DBConnection.java
+++ src/main/java/com/pharmacy/pharmacymanagementsystem/util/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/pharmacy_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "YOUR_MYSQL_USERNAME";
+    private static final String PASSWORD = "YOUR_MYSQL_PASSWORD";
 
     public static Connection getConnection() {
         try {
